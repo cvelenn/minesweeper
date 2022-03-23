0ab7d70e77e7ae6ce5c4d91007f4a7a502ef238e
Cvetko Zubanov <cvetko.zubanov@servalit.com>
2022-03-23 21:06:35 +0100

    fix

--- src/features/field/Filed.css.ts
+++ src/features/field/Filed.css.ts
@@ -1,9 +1,5 @@
 import { makeStyles } from '@mui/styles';
 
-export interface FiledCSSProps {
-    marked: boolean;
-}
-
 export const useStyles = makeStyles((theme) => ({
     field: {
         cursor: 'pointer',
