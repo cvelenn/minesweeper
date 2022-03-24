3c0fb8488956b0bf8e5e78699f1a40c73d7ab480
Cvetko Zubanov <cvetko.zubanov@servalit.com>
2022-03-24 13:52:01 +0100

    fix

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,3 +1,3 @@
-const ws = new WebSocket(process.env.REACT_APP_WEB_SOCKET_URL!);
+const ws = new WebSocket(process.env.REACT_APP_WEB_SOCKET_URL! || "wss://hometask.eg1236.com/game1/");
 
 export default ws;
